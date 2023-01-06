edd2fed9475e4b1a5c9fbfa0590cab72b45eb04f
madhan <manmadhan177@gmail.com>
2023-01-06 12:43:03 +0530

    page under construction

--- apps/users/src/app/utils/display-panel/display-panel.component.ts
+++ apps/users/src/app/utils/display-panel/display-panel.component.ts
@@ -1,8 +1,37 @@
 import { Component } from '@angular/core';
+import { trigger, state, style, transition, animate } from '@angular/animations';
 
 @Component({
   selector: 'washly-display-panel',
   templateUrl: './display-panel.component.html',
   styleUrls: ['./display-panel.component.css'],
+   animations: [
+    trigger('moveText', [
+      state('left', style({ transform: 'translateX(-100%)' })),
+      state('right', style({ transform: 'translateX(100%)' })),
+      transition('* => *', animate(20000))
+    ])
+  ]
 })
-export class DisplayPanelComponent {}
+export class DisplayPanelComponent {
+  texts = ['Page under construction'];
+  
+  currentText = '';
+  index = 0;
+  state = 'left';
+  interval: NodeJS.Timer | undefined;
+
+  ngOnInit() {
+    this.interval = setInterval(() => {
+      this.currentText = this.texts[this.index];
+      this.state = this.state === 'left' ? 'right' : 'left';
+      this.index = (this.index + 1) % this.texts.length;
+    }, 1000);
+  }
+}
+
+
+
+
+
+
