c3db1c16bd060c7daa32c4c5ca6279176d330d9c
Harish A <harish18092002@gmail.com>
2023-01-22 13:04:17 +0530

    customer page

--- apps/users/src/app/utils/customer-details/customer-details.component.ts
+++ apps/users/src/app/utils/customer-details/customer-details.component.ts
@@ -6,9 +6,7 @@ import { Component } from '@angular/core';
   styleUrls: ['./customer-details.component.css'],
 })
 export class CustomerDetailsComponent {
-  dataSource = [
-    { serviceName: 'Laundry', noOfItems: 20, weightOfItems: 10, amount: 200 },
-  ];
+
 
 
   customers = [
@@ -44,339 +42,251 @@ export class CustomerDetailsComponent {
           title: 'Phone number',
           value: '9790909162',
         },
-        {
-          title: 'Delivery timeslot',
-          value: '8AM - 10AM',
-        },
+     
      
       ],
       address: [
    {
-          title: 'Pickup address',
+          title: 'Home',
           value: `14/A, East coast street, South west
           Kotturpalayamkotai, Vikasvendra kundramma puram,
           Tirunelveli. 627422. Opposite: Olala High-class Non-veg Restaurant.  `,
         },
         {
-          title: 'Delivery address',
+          title: 'Office',
           value: `14/A, East coast street, South west
           Kotturpalayamkotai, Vikasvendra kundramma puram,
           Tirunelveli. 627422. Opposite: Olala High-class Non-veg Restaurant.   `,
         },
       ],
-      bill: {
-        totalServiceAmount: '342',
-        deliveryFee: '50',
-        tax: '23',
-        totalAmount: '415',
-      },
     },
-    {
-      bookingId: 'X78976TY6546I',
+
+    // 2nd customer
+
+     {
+      customerId: 'X78976TY6546I',
       cardData: {
-        bookingId: 'X78976TY6546I',
-        storeName: 'Guduvanchery washly',
-        date: '2/1/2023',
-        selected: false,
+        Name: 'Abin',
+        no: '9987654321',
+        selected: true,
       },
-      bookingDetails: [
-        {
-          title: 'Booking id',
-          value: 'X78976TY6546I',
-        },
+      customerDetails: [
         {
           title: 'Customer name',
-          value: 'Sivaraman S',
-        },
-        {
-          title: 'Date',
-          value: '2/1/2023',
-        },
-        {
-          title: 'Store name',
-          value: 'thambaram washly',
+          value: 'Abin',
         },
         {
           title: 'Status',
-          value: 'PENDING APPROVAL',
+          value: 'Active',
         },
+        {
+          title: 'Customer ID',
+          value: 'X78976TY6546I',
+        },
+        {
+          title: 'Store name',
+          value: 'Guduvanchery',
+        },
+       
       ],
-      pickUpAndDeliveryDetails: [
+      contact: [
        
         {
-          title: 'Pickup timeslot',
-          value: '8AM - 10AM',
+          title: 'Phone number',
+          value: '9987654321',
         },
-        {
-          title: 'Delivery timeslot',
-          value: '10AM - 12AM',
-        },
-        {
-          title: 'Pickup address',
+     
+     
+      ],
+      address: [
+   {
+          title: 'Home',
           value: `14/A, East coast street, South west
           Kotturpalayamkotai, Vikasvendra kundramma puram,
           Tirunelveli. 627422. Opposite: Olala High-class Non-veg Restaurant.  `,
         },
         {
-          title: 'Delivery address',
+          title: 'Office',
           value: `14/A, East coast street, South west
           Kotturpalayamkotai, Vikasvendra kundramma puram,
           Tirunelveli. 627422. Opposite: Olala High-class Non-veg Restaurant.   `,
         },
       ],
-      selectedService: [
-        {
-          name: 'Regular Wash',
-          numberOfItems: 7,
-          weight: '12',
-          amount: '122',
-        },
-        {
-          name: 'Ironing',
-          numberOfItems: 7,
-          weight: '21',
-          amount: '232',
-        },
-        {
-          name: 'Dry Wash',
-          numberOfItems: 7,
-          weight: '12',
-          amount: '121',
-        },
-      ],
-      bill: {
-        totalServiceAmount: '342',
-        deliveryFee: '50',
-        tax: '23',
-        totalAmount: '415',
-      },
     },
-    {
-      bookingId: 'X78976TY6546I',
+
+
+    // 3rdcustomer
+
+     {
+      customerId: 'X78976TY6546I',
       cardData: {
-        bookingId: 'X78976TY6546I',
-        storeName: 'Guduvanchery washly',
-        date: '2/1/2023',
-        selected: false,
+        Name: 'Arun',
+        no: '1234567890',
+        selected: true,
       },
-      bookingDetails: [
-        {
-          title: 'Booking id',
-          value: 'X78976TY6546I',
-        },
+      customerDetails: [
         {
           title: 'Customer name',
-          value: 'Sivaraman S',
-        },
-        {
-          title: 'Date',
-          value: '2/1/2023',
-        },
-        {
-          title: 'Store name',
-          value: 'Guduvanchery washly',
+          value: 'Arun',
         },
         {
           title: 'Status',
-          value: 'PENDING APPROVAL',
-        },
-      ],
-      pickUpAndDeliveryDetails: [
-      
-        {
-          title: 'Pickup timeslot',
-          value: '8AM - 10AM',
+          value: 'Active',
         },
         {
-          title: 'Delivery timeslot',
-          value: '8AM - 10AM',
-        },
-        {
-          title: 'Pickup address',
-          value: `14/A, East coast street, South west
-          Kotturpalayamkotai, Vikasvendra kundramma puram,
-          Tirunelveli. 627422. Opposite: Olala High-class Non-veg Restaurant.  `,
-        }, 
-        {
-          title: 'Delivery address',
-          value: `14/A, East coast street, South west
-          Kotturpalayamkotai, Vikasvendra kundramma puram,
-          Tirunelveli. 627422. Opposite: Olala High-class Non-veg Restaurant.   `,
-        },
-      ],
-      selectedService: [
-        {
-          name: 'Regular Wash',
-          numberOfItems: 7,
-          weight: '12',
-          amount: '122',
-        },
-        {
-          name: 'Ironing',
-          numberOfItems: 7,
-          weight: '21',
-          amount: '232',
-        },
-        {
-          name: 'Dry Wash',
-          numberOfItems: 7,
-          weight: '12',
-          amount: '121',
-        },
-      ],
-      bill: {
-        totalServiceAmount: '342',
-        deliveryFee: '50',
-        tax: '23',
-        totalAmount: '415',
-      },
-    },
-    {
-      bookingId: 'X78976TY6546I',
-      cardData: {
-        bookingId: 'X78976TY6546I',
-        storeName: 'Guduvanchery washly',
-        date: '2/1/2023',
-        selected: false,
-      },
-      bookingDetails: [
-        {
-          title: 'Booking id',
+          title: 'Customer ID',
           value: 'X78976TY6546I',
         },
         {
-          title: 'Customer name',
-          value: 'Sivaraman S',
-        },
-        {
-          title: 'Date',
-          value: '2/1/2023',
-        },
-        {
           title: 'Store name',
-          value: 'Guduvanchery washly',
+          value: 'Guduvanchery',
         },
-        {
-          title: 'Status',
-          value: 'PENDING APPROVAL',
-        },
+       
       ],
-      pickUpAndDeliveryDetails: [
-        
+      contact: [
+       
         {
-          title: 'Pickup timeslot',
-          value: '8AM - 10AM',
+          title: 'Phone number',
+          value: '1234567890',
         },
-        {
-          title: 'Delivery timeslot',
-          value: '8AM - 10AM',
-        },
-        {
-          title: 'Pickup address',
+     
+     
+      ],
+      address: [
+   {
+          title: 'Home',
           value: `14/A, East coast street, South west
           Kotturpalayamkotai, Vikasvendra kundramma puram,
           Tirunelveli. 627422. Opposite: Olala High-class Non-veg Restaurant.  `,
         },
         {
-          title: 'Delivery address',
+          title: 'Office',
           value: `14/A, East coast street, South west
           Kotturpalayamkotai, Vikasvendra kundramma puram,
           Tirunelveli. 627422. Opposite: Olala High-class Non-veg Restaurant.   `,
         },
       ],
-      selectedService: [
+    },
+
+
+    // 4th customer
+
+     {
+      customerId: 'X78976TY6546I',
+      cardData: {
+        Name: 'Madan',
+        no: '9234567890',
+        selected: true,
+      },
+      customerDetails: [
         {
-          name: 'Regular Wash',
-          numberOfItems: 7,
-          weight: '12',
-          amount: '122',
+          title: 'Customer name',
+          value: 'Madan',
         },
         {
-          name: 'Ironing',
-          numberOfItems: 7,
-          weight: '21',
-          amount: '232',
+          title: 'Status',
+          value: 'Active',
         },
         {
-          name: 'Dry Wash',
-          numberOfItems: 7,
-          weight: '12',
-          amount: '121',
+          title: 'Customer ID',
+          value: 'X78976TY6546I',
+        },
+        {
+          title: 'Store name',
+          value: 'Guduvanchery',
+        },
+       
+      ],
+      contact: [
+       
+        {
+          title: 'Phone number',
+          value: '9234567890',
+        },
+     
+     
+      ],
+      address: [
+   {
+          title: 'Home',
+          value: `14/A, East coast street, South west
+          Kotturpalayamkotai, Vikasvendra kundramma puram,
+          Tirunelveli. 627422. Opposite: Olala High-class Non-veg Restaurant.  `,
+        },
+        {
+          title: 'Office',
+          value: `14/A, East coast street, South west
+          Kotturpalayamkotai, Vikasvendra kundramma puram,
+          Tirunelveli. 627422. Opposite: Olala High-class Non-veg Restaurant.   `,
         },
       ],
-      bill: {
-        totalServiceAmount: '342',
-        deliveryFee: '50',
-        tax: '23',
-        totalAmount: '415',
+    },
+
+      {
+      customerId: 'X78976TY6546I',
+      cardData: {
+        Name: 'Nivetha',
+        no: '1234567890',
+        selected: true,
       },
+      customerDetails: [
+        {
+          title: 'Customer name',
+          value: 'Nivetha',
+        },
+        {
+          title: 'Status',
+          value: 'Active',
+        },
+        {
+          title: 'Customer ID',
+          value: 'X78976TY6546I',
+        },
+        {
+          title: 'Store name',
+          value: 'Guduvanchery',
+        },
+       
+      ],
+      contact: [
+       
+        {
+          title: 'Phone number',
+          value: '1234567890',
+        },
+     
+     
+      ],
+      address: [
+   {
+          title: 'Home',
+          value: `14/A, East coast street, South west
+          Kotturpalayamkotai, Vikasvendra kundramma puram,
+          Tirunelveli. 627422. Opposite: Olala High-class Non-veg Restaurant.  `,
+        },
+        {
+          title: 'Office',
+          value: `14/A, East coast street, South west
+          Kotturpalayamkotai, Vikasvendra kundramma puram,
+          Tirunelveli. 627422. Opposite: Olala High-class Non-veg Restaurant.   `,
+        },
+      ],
     },
-  ];
-  selectedBooking = this.customers[0];
 
-  editMode = false;
-
-  items = [
-    { name: 'Dry wash', quantity: 5, weight: '10', amount: 10, editing: false },
-    { name: 'Ironing', quantity: 3, weight: '15', amount: 10, editing: false },
-    { name: 'Washing', quantity: 3, weight: '15', amount: 10, editing: false },
-    {
-      name: 'Washing & Ironing',
-      quantity: 3,
-      weight: '15',
-      amount: 10,
-      editing: false,
-    },
-    {
-      name: 'Dry wash & Ironing',
-      quantity: 3,
-      weight: '15',
-      amount: 10,
-      editing: false,
-    },
-    {
-      name: 'Dry wash & Washing',
-      quantity: 3,
-      weight: '15',
-      amount: 10,
-      editing: false,
-    },
-    {
-      name: 'Dry wash & Ironing & Washing',
-      quantity: 3,
-      weight: '15',
-      amount: 10,
-      editing: false,
-    },
-  ];
-
-  partner: string[] = [
-    'Arunkumar',
-    'Abinpaul',
-    'Aniruth',
-    'Dhoni',
-    'Madhan',
-    'Siva',
-    'Siva Ram',
-  ];
+  ]
+ 
+   
+  selectedcustomer = this.customers[0];
 
 
 
 
-  total = 0;
 
-  ngOnIt() {
-    this.dataSource.forEach((item) => {
-      if (!item.weightOfItems) item.weightOfItems = 0 as any;
-    });
-  }
 
-  getTotalAmount() {
-    this.total = 0;
-    for (const item of this.items) {
-      this.total += item.amount;
-    }
-  }
+
+  
+
+
 
   // objects = [
   //   {billTag: 'Total service amount', billAmount: '20',editing:false},
@@ -385,18 +295,7 @@ export class CustomerDetailsComponent {
   //   {billTag: 'Amount', billAmount: '20',editing:false},
   // ]
   // eslint-disable-next-line @typescript-eslint/member-ordering
-  text = 'Arun';
-  editModeOn() {
-    this.editMode = true;
-  }
-
-  editModeOff() {
-    this.editMode = false;
-  }
-
-  save() {
-    this.editMode = false;
-  }
+  
   // eslint-disable-next-line @typescript-eslint/member-ordering
   // showButton = false;
   // toggleButton() {
@@ -406,12 +305,12 @@ export class CustomerDetailsComponent {
   showElements = false;
   // eslint-disable-next-line @typescript-eslint/member-ordering
   showListElements = false;
-  setSelectedBooking(booking: any) {
-    this.customers.forEach((booking) => {
-      booking.cardData.selected = false;
+  setSelectedcustomer(customer: any) {
+    this.customers.forEach((customer) => {
+      customer.cardData.selected = false;
     });
-    booking.cardData.selected = true;
-    this.selectedBooking = booking;
+    customer.cardData.selected = true;
+    this.selectedcustomer = customer;
   }
 }
 
